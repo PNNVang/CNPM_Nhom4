cda665808baaa6924e00e6f06cb0640e97f92a62
AnhXuan04 <22130333@st.hcmuaf.edu.vn>
2025-05-17 02:04:36 +0700

    Hoàn thiện thêm sản phẩm cho admin

--- Controller/ProductController.cs
+++ Controller/ProductController.cs
@@ -19,15 +19,18 @@ public class ProductController : Microsoft.AspNetCore.Mvc.Controller
         return Ok(products);
     }
 
+    //6.1: Admin gửi yêu cầu thêm sản phẩm mới
     [HttpPost("insertproduct")]
     public async Task<IActionResult> AddProduct(ProductForm model)
     {
         var isAdded = await _productService.AddProductAsync(model);
         if (!isAdded)
         {
+            //6.1.7: Thêm không thành công sản phẩm 
             return BadRequest(new { message = "Failed to add product" });
         }
 
+        //6.1.6: Thêm thành công sản phẩm 
         return Ok(new { message = "Product added successfully" });
     }
 }
--- Service/ProductManagementService.cs
+++ Service/ProductManagementService.cs
@@ -30,8 +30,11 @@ public class ProductManagementService
     }
     public async Task<bool> AddProductAsync(ProductForm model)
     {
+        //6.1.1: Khởi tạo id tự động tăng 
         var newProductId = _context.Products.Any() ? _context.Products.Max(x => x.Id) + 1 : 1;
         var newProductImageId = _context.ProductImages.Any() ? _context.ProductImages.Max(x => x.Id) + 1 : 1;
+
+        //6.1.2: Tạo đối tượng sản phẩm 
         var product = new Product
         {
             Id = newProductId,
@@ -46,9 +49,11 @@ public class ProductManagementService
             Description = model.Description,
         };
 
+        //6.1.3: Lưu sản phẩm vào bảng Product trong database
         _context.Products.Add(product);
         await _context.SaveChangesAsync();
 
+        //6.1.4: Upload các ảnh sản phẩm lên Cloudinary và nhận về URL
         var imageUrls = await UploadImagesToCloudinary(model);
         var productImage = new ProductImage
         {
@@ -60,6 +65,7 @@ public class ProductManagementService
             Img4 = imageUrls[4]
         };
 
+        //6.1.5: Lưu thông tin ảnh sản phẩm vào database
         _context.ProductImages.Add(productImage);
         await _context.SaveChangesAsync();
 
