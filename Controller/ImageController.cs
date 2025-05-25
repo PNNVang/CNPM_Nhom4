59432c2dd5afac605b63c4544b29ce2f990f93eb
Khoa <21130402@st.hcmuaf.edu.vn>
2025-05-25 14:02:12 +0700

    Khoa

--- Controller/ImageController.cs
+++ Controller/ImageController.cs
@@ -1,42 +1,47 @@
-﻿using CloudinaryDotNet;
+﻿using CloudinaryDotNet; 
 using CloudinaryDotNet.Actions;
-using Dot_Net_ECommerceWeb.DBContext;
+using Dot_Net_ECommerceWeb.DBContext; 
 using Dot_Net_ECommerceWeb.Model;
 using Dot_Net_ECommerceWeb.Utils;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore; 
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace Dot_Net_ECommerceWeb.Controller;
-
-[ApiController]
-[Route("/api/[controller]")]
-public class ImageController : Microsoft.AspNetCore.Mvc.Controller
+namespace Dot_Net_ECommerceWeb.Controller
 {
-    private readonly AppDBContext _dbContext;
-
-    public ImageController(AppDBContext dbContext)
+    // Đánh dấu đây là một API Controller, route là /api/image
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ImageController : Microsoft.AspNetCore.Mvc.Controller
     {
-        _dbContext = dbContext;
-    }
+        private readonly AppDBContext _dbContext;
 
-    //api lay thong tin anh
-    [HttpGet("getimage")]
-    public async Task<ActionResult<ProductImage>> GetImage()
-    {
-        var image = (from p in _dbContext.Products
-            join imgproduct in _dbContext.ProductImages on p.Id equals imgproduct.Id select new
-            {
-                imgproduct.Id,
-                p.ProductName,
-                imgproduct.ImgMain,
-                imgproduct.Img1,
-                imgproduct.Img2,
-                imgproduct.Img3,
-                imgproduct.Img4,
-            });
-        return Ok(image);
-    }
+        // Constructor nhận AppDBContext để truy cập dữ liệu từ CSDL
+        public ImageController(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
-  
+        // API để lấy thông tin ảnh sản phẩm
+        [HttpGet("getimage")]
+        public async Task<ActionResult<ProductImage>> GetImage()
+        {
+            // Truy vấn kết hợp bảng Products và ProductImages
+            var image = (from p in _dbContext.Products
+                         join imgproduct in _dbContext.ProductImages on p.Id equals imgproduct.Id
+                         select new
+                         {
+                             imgproduct.Id,
+                             p.ProductName,
+                             imgproduct.ImgMain,
+                             imgproduct.Img1,
+                             imgproduct.Img2,
+                             imgproduct.Img3,
+                             imgproduct.Img4,
+                         });
+
+            // Trả kết quả dạng HTTP 200 OK với danh sách ảnh
+            return Ok(image);
+        }
+    }
 }
--- Controller/InventoryController.cs
+++ Controller/InventoryController.cs
@@ -1,26 +1,34 @@
-﻿using Dot_Net_ECommerceWeb.DBContext;
-using Dot_Net_ECommerceWeb.Model;
-using Dot_Net_ECommerceWeb.Service;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
+﻿using Dot_Net_ECommerceWeb.DBContext; 
+using Dot_Net_ECommerceWeb.Model; 
+using Dot_Net_ECommerceWeb.Service; 
+using Microsoft.AspNetCore.Http.HttpResults; 
+using Microsoft.AspNetCore.Mvc; 
 using Microsoft.EntityFrameworkCore;
 
-namespace Dot_Net_ECommerceWeb.Controller;
-[Route("/api/[controller]")]
-[ApiController]
-public class InventoryController:Microsoft.AspNetCore.Mvc.Controller
+namespace Dot_Net_ECommerceWeb.Controller
 {
-    private readonly InventoryService _inventoryService;
+    // Định nghĩa route cho controller: /api/inventory
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class InventoryController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly InventoryService _inventoryService;
 
-    public InventoryController(InventoryService inventoryService)
-    {
-        _inventoryService = inventoryService;
-    }
-//api lay thong tin so luong san pham
-    [HttpGet("getinventoriesdetail")]
-    public async Task<IActionResult> GetInventory()
-    {
-        var inventories = await _inventoryService.GetInventoryDetailsAsync();
-        return Ok(inventories);
+        // Constructor nhận vào dịch vụ quản lý tồn kho thông qua Dependency Injection
+        public InventoryController(InventoryService inventoryService)
+        {
+            _inventoryService = inventoryService;
+        }
+
+        // API lấy thông tin số lượng sản phẩm trong kho
+        [HttpGet("getinventoriesdetail")]
+        public async Task<IActionResult> GetInventory()
+        {
+            // Gọi service để lấy thông tin tồn kho
+            var inventories = await _inventoryService.GetInventoryDetailsAsync();
+            
+            // Trả về dữ liệu dưới dạng HTTP 200 OK với dữ liệu tồn kho
+            return Ok(inventories);
+        }
     }
 }
